f2f7635ec16503ed95b6aac7140059809737639a
Ana Paula Oliva <56927809+anapaulaoliva@users.noreply.github.com>
2021-09-22 11:39:01 -0500

    Testing capabilities with a manual urls array

--- NodeBasic/puppeteerPENNC.js
+++ NodeBasic/puppeteerPENNC.js
@@ -2,38 +2,52 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    //imdb example url
-    const URL = 'https://upenn.technologypublisher.com/searchresults.aspx?q=&type=&page=1sort=datecreated&order=desc';
-    //initialize browser & puppeteer library
-    //headless to open the browser
-    const browser = await puppeteer.launch({headless:false});
-    const page = await browser.newPage();
-    //making sure everything has loaded before scraping - telling the browser the navigation loading is finished
-    await page.goto(URL, { waitUntil: 'networkidle2' });
+    const TEST_URLS = [
+        'https://upenn.technologypublisher.com/technology/44262',
+        'https://upenn.technologypublisher.com/technology/44237',
+        'https://upenn.technologypublisher.com/technology/44818', 
+        'https://upenn.technologypublisher.com/technology/44814',
+        'https://upenn.technologypublisher.com/technology/44234', 
+        'https://upenn.technologypublisher.com/technology/44057',
+        'https://upenn.technologypublisher.com/technology/43801' 
+    ];
 
-    //like opening devtools console
-
-    //let data = await page.evaluate(() => {
+    for(let i=0; i<=6; i++){
+        let URL = TEST_URLS[i];
+        //initialize browser & puppeteer library
+        //headless to open the browser
+        const browser = await puppeteer.launch({headless:false, slowMo: 250, defaultViewport: null});
+        //redirect steps here
+        const page = await browser.newPage();
+        //making sure everything has loaded before scraping - telling the browser the navigation loading is finished
+        await page.goto(URL, { waitUntil: 'networkidle0' });
+        //page.waitForNavigation  
+        //await page.waitForSelector(() => document.querySelector('#main-content'));
+        //await page.waitForXPath('//*[@id="formTechPub1"]/div');
+        //like opening devtools console
+        let data = await page.evaluate(() => {
         //declaring elements variables
-      //  let title = await page.title();
-        //let title = document.querySelector("#formTechPub1 > div > table:nth-child(3) > tbody > tr:nth-child(1) > td > h2 > a").innerHTML;
-        // let publishedDate = document.querySelector('#formTechPub1 > div > table:nth-child(3) > tbody > tr:nth-child(1) > td > h3 > span').innerText; 
-       //let articleURL = document.querySelector('#formTechPub1 > div > table:nth-child(3) > tbody > tr:nth-child(1) > td > h2 > a').href;
-        //let articleURL = page.URL();
-        //return {
-          //  title, 
-            //publishedDate,
-            //articleURL
-        //}
-   // });
+        let title = document.querySelector("#formTechPub1 > div > table > tbody > tr > td:nth-child(1) > h1").innerHTML;
+        let publishedDate = document.querySelector('#formTechPub1 > div > table > tbody > tr > td:nth-child(1) > div:nth-child(4) > div:nth-child(1)').innerText; 
+        let articleURL = document.querySelector("#formTechPub1 > div > table > tbody > tr > td:nth-child(1) > h1").href;
+
+    
+        return {
+            title, 
+            publishedDate,
+            articleURL
+        }
+        });
+    }
+    
 
     // print data object
-    //console.log(data);
+    console.log(data);
     let title = await page.title();
     let url = await page.url();
     console.log(title,url);
     //set a simple debugger to inspect data
-    //debugger;
+    debugger;
 
     //close browser
     await browser.close();
