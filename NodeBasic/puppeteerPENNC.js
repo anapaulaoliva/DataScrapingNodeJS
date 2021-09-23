7d68c836836453917291065968729fc0a2f46632
Ana Paula Oliva <56927809+anapaulaoliva@users.noreply.github.com>
2021-09-23 10:32:50 -0500

    Extracting only art titles

--- NodeBasic/puppeteerPENNC.js
+++ NodeBasic/puppeteerPENNC.js
@@ -11,52 +11,55 @@ const puppeteer = require('puppeteer');
         'https://upenn.technologypublisher.com/technology/44057',
         'https://upenn.technologypublisher.com/technology/43801' 
     ];
-
+    let scraping_data = [];
     for(let i=0; i<=6; i++){
         let URL = TEST_URLS[i];
+
         //initialize browser & puppeteer library
         //headless to open the browser
-        const browser = await puppeteer.launch({headless:false, slowMo: 250, defaultViewport: null});
+        const browser = await puppeteer.launch({headless:false, /*slowMo: 250,*/ defaultViewport: null/*, devtools:true*/});
         //redirect steps here
         const page = await browser.newPage();
         //making sure everything has loaded before scraping - telling the browser the navigation loading is finished
         await page.goto(URL, { waitUntil: 'networkidle0' });
+
+        
         //page.waitForNavigation  
         //await page.waitForSelector(() => document.querySelector('#main-content'));
         //await page.waitForXPath('//*[@id="formTechPub1"]/div');
         //like opening devtools console
+
+	//  ------------- Article data
         let data = await page.evaluate(() => {
         //declaring elements variables
-        let title = document.querySelector("#formTechPub1 > div > table > tbody > tr > td:nth-child(1) > h1").innerHTML;
-        let publishedDate = document.querySelector('#formTechPub1 > div > table > tbody > tr > td:nth-child(1) > div:nth-child(4) > div:nth-child(1)').innerText; 
-        let articleURL = document.querySelector("#formTechPub1 > div > table > tbody > tr > td:nth-child(1) > h1").href;
 
-    
+	//  ------------- Add Title
+        let title = document.querySelector("#formTechPub1 > div > table > tbody > tr > td:nth-child(1) > h1").innerHTML;
+    //  ------------- Add URL
+        let url = document.querySelector("#formTechPub1 > div > table > tbody > tr > td:nth-child(1) > div.c_tp_direct_link > a").innerText    //  ------------- Add Title
+
         return {
-            title, 
-            publishedDate,
-            articleURL
+            'title': title,
+            //'articleURL': URL
         }
         });
-    }
-    
+        scraping_data.push(data);
+        await browser.close();
 
-    // print data object
-    console.log(data);
-    let title = await page.title();
-    let url = await page.url();
-    console.log(title,url);
+    }
+     // print data object
+    console.log(scraping_data);
     //set a simple debugger to inspect data
     debugger;
 
     //close browser
-    await browser.close();
-
+    //await browser.close();
 })();
 
 
 //$ npm i progress
 var ProgressBar = require('progress');
+const { add } = require('cheerio/lib/api/traversing');
 
 var bar = new ProgressBar(':bar :current/:total', { total: 10 });
 var timer = setInterval(function () {
