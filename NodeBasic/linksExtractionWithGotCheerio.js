7b4d1a97d4a62a46838d61a5d4fc18da8e04a981
Ana Paula Oliva <56927809+anapaulaoliva@users.noreply.github.com>
2021-09-29 00:38:49 -0500

    Update links extractor

--- NodeBasic/linksExtractionWithGotCheerio.js
+++ NodeBasic/linksExtractionWithGotCheerio.js
@@ -1,15 +1,23 @@
+/* Use the keyword 'require' to import modules and assign it to a constant or variable
+https://www.freecodecamp.org/news/requiring-modules-in-node-js-everything-you-need-to-know-e7fbd119be8/
+ To use third-party modules such as puppeteer, they must be installed locally (e.g. 'npm i puppeteer')
+ If a package.json exists, all dependencies can be installed with 'npm install'
+*/
+//Human-friendly and powerful HTTP request library for Node.js
 const got = require('got');
+//Cheerio parses markup and provides an API for traversing/manipulating the resulting data structure. 
 const cheerio = require('cheerio');
+const ProgressBar = require('progress');
 const { link } = require('fs');
 const PAGE_URLS = [];
-let url;
 
-const extractLinks = async (PAGE_URLS, url, pages) => {
+const extractLinks = async(PAGE_URLS, pages) => {
 
     try {
-        for(let i=0; i<=pages; i++){
-            url = 'https://upenn.technologypublisher.com/searchresults.aspx?q=&page='+i+'&sort=datecreated&order=desc';
-        
+        let url;
+        for (let i = 0; i <= pages; i++) {
+            url = 'https://upenn.technologypublisher.com/searchresults.aspx?q=&page=' + i + '&sort=datecreated&order=desc';
+
             // Fetching HTML
             const response = await got(url);
             const html = response.body;
@@ -28,12 +36,10 @@ const extractLinks = async (PAGE_URLS, url, pages) => {
                 );
             });
 
-            
-        }
-        console.log(filterLinks(PAGE_URLS));
 
-        console.log(filterLinks(PAGE_URLS).length);
-    
+        }
+        return (filterLinks(PAGE_URLS));
+
         // do something else here with these links, such as writing to a file or saving them to your database
 
     } catch (error) {
@@ -46,9 +52,19 @@ const filterLinks = (linksArr) => {
     return linksArr.filter(element => element.match(regEx));
 }
 
-extractLinks(PAGE_URLS, url, 5);
+/* 
+Modules need to be exported so they can be imported using the 'require' keyword
+1· https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
+2· https://stackoverflow.com/questions/16631064/declare-multiple-module-exports-in-node-js#16631079
+*/
+module.exports = extractLinks;
 
-
-
-
-
+var bar = new ProgressBar(':bar :current/:total', { total: 10 });
+var timer = setInterval(function() {
+    bar.tick();
+    if (bar.complete) {
+        clearInterval(timer);
+    } else if (bar.curr === 5) {
+        bar.interrupt('Data scraping...\ncurrent progress is ' + bar.curr + '/' + bar.total);
+    }
+}, 1000);
